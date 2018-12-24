59eee4962263442674e6e9c045f8b4af82f1bece
dinopuguh <puguhmunung16@gmail.com>
2018-12-24 21:44:42 +0700

    react chartjs with pusher in alpha

--- src/views/Alpha/SoilChart.js
+++ src/views/Alpha/SoilChart.js
@@ -3,12 +3,19 @@ import { Line } from 'react-chartjs-2';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import axios from 'axios';
+import Pusher from 'pusher-js';
 
 const brandPrimary = getStyle('--primary')
 const brandSuccess = getStyle('--success')
 const brandInfo = getStyle('--info')
 const brandWarning = getStyle('--warning')
 const brandDanger = getStyle('--danger')
+
+const pusher = new Pusher('b01fb79d33e790f8c38d', {
+    cluster: 'ap1',
+    encrypted: true
+});
+const channel = pusher.subscribe('alpha');
 
 export default class SoilChart extends Component
 {
@@ -63,7 +70,8 @@ export default class SoilChart extends Component
     }
 
     async componentDidMount() {
-        await axios.get('http://localhost:5000/api/v1/alpha')
+
+        await axios.get('https://aizd-webservice.herokuapp.com/api/v1/alpha')
         .then(res => {
             const alpha = res.data[0];
             let temperature = [];
@@ -95,6 +103,103 @@ export default class SoilChart extends Component
                     tds.push(element)
                 });
 
+                alpha.soil_epsilon.forEach(element => {
+                    epsilon.push(element)
+                });
+    
+                alpha.created_at.forEach(element => {
+                    created_at.push(element)
+                });
+            }
+
+            this.setState({
+                Data : {
+                    labels: created_at,
+                    datasets: [
+                        {
+                            label: 'Temperature',
+                            backgroundColor: hexToRgba(brandInfo, 10),
+                            borderColor: brandInfo,
+                            pointHoverBackgroundColor: '#fff',
+                            borderWidth: 2,
+                            data: temperature,
+                        },
+                        {
+                            label: 'VWC',
+                            backgroundColor: hexToRgba(brandDanger, 10),
+                            borderColor: brandDanger,
+                            pointHoverBackgroundColor: '#fff',
+                            borderWidth: 2,
+                            data: vwc,
+                        },
+                        {
+                            label: 'EC',
+                            backgroundColor: hexToRgba(brandWarning, 10),
+                            borderColor: brandWarning,
+                            pointHoverBackgroundColor: '#fff',
+                            borderWidth: 2,
+                            data: ec,
+                        },
+                        {
+                            label: 'Salinity',
+                            backgroundColor: hexToRgba(brandSuccess, 10),
+                            borderColor: brandSuccess,
+                            pointHoverBackgroundColor: '#fff',
+                            borderWidth: 2,
+                            data: salinity,
+                        },
+                        {
+                            label: 'TDS',
+                            backgroundColor: hexToRgba(brandPrimary, 10),
+                            borderColor: brandPrimary,
+                            pointHoverBackgroundColor: '#fff',
+                            borderWidth: 2,
+                            data: tds,
+                        },
+                        {
+                            label: 'Epsilon',
+                            backgroundColor: hexToRgba('#42EEF4', 10),
+                            borderColor: '#42EEF4',
+                            pointHoverBackgroundColor: '#fff',
+                            borderWidth: 2,
+                            data: epsilon,
+                        }
+                    ]
+                }
+            })
+        })
+
+        await channel.bind('update-alpha', data => {
+            const alpha = data;
+            let temperature = [];
+            let vwc = [];
+            let ec = [];
+            let salinity = [];
+            let tds = [];
+            let epsilon = [];
+            let created_at = [];
+
+            if(alpha) {
+                alpha.soil_temperature.forEach(element => {
+                    temperature.push(element)
+                });
+    
+                alpha.soil_vwc.forEach(element => {
+                    vwc.push(element)
+                });
+    
+                alpha.soil_ec.forEach(element => {
+                    ec.push(element)
+                });
+    
+                alpha.soil_salinity.forEach(element => {
+                    salinity.push(element)
+                });
+    
+                alpha.soil_tds.forEach(element => {
+                    tds.push(element)
+                });
+    
                 alpha.soil_epsilon.forEach(element => {
                     epsilon.push(element)
                 });
